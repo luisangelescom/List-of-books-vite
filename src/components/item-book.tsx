07d3e4e4a75b4f8e0591ca80980877c31266079e
luisangelescom <luis20.lgi@gmail.com>
2023-07-27 16:22:23 -0600

    Se hace fix en animate al agregar libros o eliminar

--- src/components/item-book.tsx
+++ src/components/item-book.tsx
@@ -8,12 +8,22 @@ function ItemBook ({ book, removeItem, addBookUser, isRemove = false }: ItemBook
   const buttonContainer = useRef<HTMLDivElement>(null)
   const action = (callback: () => void, back = false): void => {
     if (buttonContainer.current != null) {
+      const { width, height, left, top } = buttonContainer.current.getBoundingClientRect()
+
       buttonContainer.current.style.zIndex = '1000'
+      buttonContainer.current.style.position = 'fixed'
+      buttonContainer.current.style.width = `${width}px`
+      buttonContainer.current.style.height = `${height}px`
+      buttonContainer.current.style.left = `${left}px`
+      buttonContainer.current.style.top = `${top}px`
+
       buttonContainer.current.animate({
         width: '100px',
         height: '100px',
-        transform: back ? 'translate(-50vw,-50vh)' : 'translate(100vw,0px)'
-      }, { duration: 300, easing: 'ease-in' }).finished.then(() => {
+        left: back ? '20%' : 'calc(100% - 120px)',
+        top: back ? '50%' : '3rem'
+        // transform: back ? 'translate(-50vw,-50vh)' : 'translate(0px,0px)'
+      }, { duration: 450, easing: 'ease-in' }).finished.then(() => {
         callback()
       }).finally(() => {})
     }
@@ -21,52 +31,56 @@ function ItemBook ({ book, removeItem, addBookUser, isRemove = false }: ItemBook
 
   return (
     <div
-      title={isRemove ? title : `Add ${title}`}
-      aria-label={title}
-      key={ISBN}
-      ref={buttonContainer}
-      className={`group w-full ${isRemove ? 'h-[260px] cursor-auto' : 'h-[350px] cursor-pointer'} items-center flex flex-col relative border-2 border-white/20 rounded-lg transition-all duration-500`}
-      onClick={() => {
-        if (addBookUser != null) {
-          action(() => addBookUser(book))
-        }
-      }}
+      className={`group w-full ${isRemove ? 'h-[260px] cursor-auto' : 'h-[350px] cursor-pointer'}`}
     >
-      {isRemove
-        ? (
-          <button
-            type='button'
-            aria-label='delete'
-            title={`Eliminar ${title}`}
-            className='absolute z-10 -top-3 -right-3 p-2 rounded-full bg-red-500'
-            onClick={() => {
-              if (removeItem != null) {
-                action(() => removeItem(ISBN), true)
+      <div
+        title={isRemove ? title : `Add ${title}`}
+        aria-label={title}
+        key={ISBN}
+        ref={buttonContainer}
+        className='w-full h-full items-center flex flex-col relative border-2 border-white/20 rounded-lg transition-all duration-500'
+        onClick={() => {
+          if (addBookUser != null) {
+            action(() => addBookUser(book))
+          }
+        }}
+      >
+        {isRemove
+          ? (
+            <button
+              type='button'
+              aria-label='delete'
+              title={`Eliminar ${title}`}
+              className='absolute z-10 -top-3 -right-3 p-2 rounded-full bg-red-500'
+              onClick={() => {
+                if (removeItem != null) {
+                  action(() => removeItem(ISBN), true)
                 // removeItem()
-              }
-            }}
-          >
-            <IconRemove />
-          </button>
-          )
-        : null}
-      <div className='flex w-full h-full relative overflow-hidden'>
-        <img
-          alt='title'
-          src={cover}
-          className='w-full aspect-square group-hover:scale-105 transition-all duration-500'
-          loading='lazy'
-        />
-      </div>
-      <div className='w-full absolute bottom-0 group-hover:h-[120px] h-[80px] transition-all duration-500 z-20'>
-        <div className='w-full h-full bg-black/60 group-hover:bg-black/80 drop-shadow-2xl flex flex-col justify-center items-center rounded-t-lg group-hover:rounded-t-none transition-all duration-500'>
-          <span className='text-white font-bold text-lg text-center group-hover:text-blue-600'>{title}</span>
-          <span className='text-white font-bold text-sm text-center group-hover:text-blue-300 opacity-0 h-0 group-hover:h-auto group-hover:opacity-100 transition-all duration-150'>
-            {genre}
-          </span>
-          <span className='text-white font-bold text-[12px] text-center group-hover:text-white opacity-0 h-0 group-hover:h-auto group-hover:opacity-100 transition-all duration-150'>
-            {year}
-          </span>
+                }
+              }}
+            >
+              <IconRemove />
+            </button>
+            )
+          : null}
+        <div className='flex w-full h-full relative overflow-hidden'>
+          <img
+            alt='title'
+            src={cover}
+            className='w-full aspect-square group-hover:scale-105 transition-all duration-500'
+            loading='lazy'
+          />
+        </div>
+        <div className='w-full absolute bottom-0 group-hover:h-[120px] h-[80px] transition-all duration-500 z-20'>
+          <div className='w-full h-full bg-black/60 group-hover:bg-black/80 drop-shadow-2xl flex flex-col justify-center items-center rounded-t-lg group-hover:rounded-t-none transition-all duration-500'>
+            <span className='text-white font-bold text-lg text-center group-hover:text-blue-600'>{title}</span>
+            <span className='text-white font-bold text-sm text-center group-hover:text-blue-300 opacity-0 h-0 group-hover:h-auto group-hover:opacity-100 transition-all duration-150'>
+              {genre}
+            </span>
+            <span className='text-white font-bold text-[12px] text-center group-hover:text-white opacity-0 h-0 group-hover:h-auto group-hover:opacity-100 transition-all duration-150'>
+              {year}
+            </span>
+          </div>
         </div>
       </div>
     </div>
